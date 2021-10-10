9a287ba991022c31caa53f0ced3130e6e673cfa3
masterflo1512 <sakharovbi@gmail.com>
2021-10-10 22:07:15 +0300

    finished bases changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 import { Header } from './components/header/Header.js'
 import { Filter } from './components/filter/Filter.js'
-import { Search } from './components/Search/Search.js'
-import { Card } from './components/card/Card.js'
+import { Search } from './components/search/Search.js'
 import { Productlist } from './components/product-list/Product-list.js'
 import { Pagination } from './components/pagination/Pagination.js'
 function App() {
   return (
-    <div class="container">
+    <div className="container">
       <Header />
-      <div class="row">
-        <div class="col-md-4 col-ms-6">
+      <div className="row">
+        <div className="col-md-4 col-ms-6">
+          <Filter />
         </div>
-        <div class="col-md-8 col-ms-6">
+        <div className="col-md-8 col-ms-6">
+          <Search />
+          <Productlist />
         </div>
       </div>
+      <Pagination />
     </div>
   );
 }
